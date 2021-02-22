36d2497b8b8357e2d803a97b442af22883c6bdc2
khalil Albert lindsay <lindsay.khalil@gmail.com>
2021-02-22 02:10:01 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -4,6 +4,6 @@ def display_board
          + "  ""  | ""  | ""  " + "\n"
          puts "-----------" + "\n"
          + " ""   | ""  | ""  " + "\n"
-    printf board
+    print display_board
 end
 
